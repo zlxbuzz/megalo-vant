d1fcb02643db8ef421663190cd9791dbab2cd571
zlxbuzz <824665478@qq.com>
2018-12-11 11:58:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# megalo-vant
+
+## 描述
+
+a megalo project
+
+## 作者
+
+zlxbuzz <824665478@qq.com>
+
+## 运行
+
+微信小程序：
+
+```bash
+npm run dev:wechat
+```
+
+支付宝小程序：
+
+```bash
+npm run dev:alipay
+```
+
+百度智能小程序：
+
+```bash
+npm run dev:swan
+```
---
+++ src/pages/my/index.js
@@ -0,0 +1,14 @@
+import App from "./my";
+import Vue from "vue";
+
+const app = new Vue(App);
+
+app.$mount();
+
+// export default {
+//   config: {
+//     usingComponents: {
+//       "van-notice-bar": "/static/vant/dist/notice-bar/index"
+//     }
+//   }
+// };
